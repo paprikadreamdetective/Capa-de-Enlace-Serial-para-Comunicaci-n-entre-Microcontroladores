55d02b31d023b08f5933eacf0e6df69795c4f980
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2023-12-18 15:03:10 -0600

    add library ssd1306.py for OLED & count num error

--- receptor/receptor.ino
+++ receptor/receptor.ino
@@ -1,10 +1,14 @@
+//#include <SoftwareSerial.h>
+//SoftwareSerial SerialACK(2, 3);
 #define BUFFER_SIZE 50
 char receivedBuffer[BUFFER_SIZE];
 int bufferIndex = 0;
 String receivedMessage;
+int error = 0;
 
 void setup() {
   Serial.begin(9600);    // Initialize the Serial monitor for debugging
+  //SerialACK.begin(9600);
   //Serial1.begin(9600);   // Initialize Serial1 for sending data
   pinMode(LED_BUILTIN, OUTPUT);      // set LED pin as output
   //digitalWrite(LED_BUILTIN, LOW);    // switch off LED pin
@@ -12,10 +16,17 @@ void setup() {
 
 void loop() {
   while (Serial.available() > 0) {
+  //while (SerialACK.available() > 0) {    
     char receivedChar = Serial.read();
     if (receivedChar == '\n') {
       Serial.print("Cadena recibida: ");
       Serial.println(receivedMessage);  // Print the received message in the Serial monitor
+      if (receivedMessage == "Hello UART!") {
+        Serial.write("ACK");
+      } else {
+        error++;
+        Serial.println("Error en transmision"); 
+      }
       digitalWrite(LED_BUILTIN, HIGH);
       delay(1.5);
       digitalWrite(LED_BUILTIN, LOW);
@@ -25,4 +36,6 @@ void loop() {
       receivedMessage += receivedChar;  // Append characters to the received message
     }
   }
+  Serial.print("Errores totales: ");
+  Serial.println(error);
 }
--- transmisor.py
+++ transmisor.py
@@ -1,24 +1,39 @@
 # UART Communication Example with Raspberry Pi Pico W
-
+import random
 import machine
 import utime
+import ssd1306
 import time 
 
 # Define UART pins
-uart = machine.UART(0, baudrate=9600, tx=0, rx=1)  # UART1, GP0 (TX) and GP1 (RX)
+#uart = machine.UART(0, baudrate=9600, tx=0, rx=1)  # UART1, GP0 (TX) and GP1 (RX)
 myLed = machine.Pin("LED", machine.Pin.OUT)
+velocidades = [9600, 19200, 38400, 57600, 115200]
+# Pantalla OLED
+i2c = machine.I2C(0, scl=machine.Pin(21), sda=machine.Pin(20), freq=400000)
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+# Limpia la pantalla
+
+message = "Alicia empezaba a estar muy cansada de estar sentada con su hermana a la orilla del rio, y de no tener nada que hacer"
 
 while True:
     # Send data
-    
-    uart.write("Hello UART!\n")
-
+    for i in range(1000):
+        uart = machine.UART(0, baudrate=velocidades[random.randint(0, 4)], tx=0, rx=1)
+        #uart.write("Hello UART!\n")
+        uart.write(message)
+        oled.fill(0)
+        oled.text("Hello MicroPython", 0, 0)
+        oled.text("Cuenta: " + str(i), 0, 20)
+        oled.show()
+        utime.sleep_ms(1)
     # Receive data
-    if uart.any():
-        received_data = uart.read()
-        print("Received:", received_data, "\n")
-        myLed.on()
-        time.sleep_ms(200)
-        myLed.off()
-        time.sleep_ms(200)
-    utime.sleep(1)
+        if uart.any():
+            received_data = uart.read()
+            print("Received:", received_data)
+            myLed.on()
+            time.sleep_ms(200)
+            myLed.off()
+            time.sleep_ms(200)
+        #utime.sleep(1)
