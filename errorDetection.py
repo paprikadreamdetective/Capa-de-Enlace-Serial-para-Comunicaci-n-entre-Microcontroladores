8c767a876ce70e0fd044d4d1fabc39dd21149f85
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2023-12-28 22:19:44 -0600

    code of errorDetection is OK

--- errorDetection.py
+++ errorDetection.py
@@ -1,55 +1,13 @@
 # -*- coding: utf-8 -*-
 import random
 
-
-def BE_msg_matrix(msg):
-    matrix = []
-    for i in msg: 
-        ascii_value = bin(ord(i))[2:]
-        if len(ascii_value) < 7:
-            missing_bits = 7 - len(ascii_value)
-            cnt = 0
-            while cnt < missing_bits:
-                ascii_value = "0" + ascii_value
-                cnt += 1
-        matrix.append(list(ascii_value))
-    return (matrix)
-
-def BE_generate_burst(matrix, a, b, burst):
-    new_matrix = [row[:] for row in matrix]
-    #new_matrix = copy.copy(matrix)
-    bits = ["0", "1"]
-    #corrupted_char = (Choices(list(range(a, b)), burst)).result
-    corrupted_char = [random.choice(list(range(a, b))) for _ in range(burst)]
-    #corrupted_char = choices.sequence(list(a, b), burst)
-    #corrupted_char = random.choice(list(a, b))
-    #corrupted_char = list.
-    #corrupted_char = random.sample(list(range(a, b)), min(burst, b - a))
-    #corrupted_char = random.choices(list(range(a, b)), k = burst)
-    #print(corrupted_char)
-    for i in range(a, b):
-        for j in range(0, len(new_matrix[0])):
-            if i in corrupted_char:
-                new_matrix[i][j] = bits[random.randint(0, 1)]
-    return (new_matrix)
-
-def BE_msg_matrix_text(matrix):
-    msg = ""
-    for i in range(0, len(matrix)):
-        character = ""
-        for j in range(0, len(matrix[0])):
-            character += matrix[i][j]
-        num = int(character, 2)
-        msg += chr(num)
-    return (msg)
-
-'''
 class BurstError:
 
-    def __init__(self):
-        self.matrix = []
-        self.matrix_corrupted = self.matrix
+    def __init__(self) -> None:
+        pass
+    
     def BE_msg_matrix(self, msg):
+        matrix = []
         for i in msg: 
             ascii_value = bin(ord(i))[2:]
             if len(ascii_value) < 7:
@@ -58,33 +16,25 @@ class BurstError:
                 while cnt < missing_bits:
                     ascii_value = "0" + ascii_value
                     cnt += 1
-            self.matrix.append(list(ascii_value))
-        return (self.matrix)
-    
-    def BE_generate_burst(self, a, b, burst):
+            matrix.append(list(ascii_value))
+        return (matrix)
+
+    def BE_generate_burst(self, matrix, a, b, burst):
+        new_matrix = [row[:] for row in matrix]
         bits = ["0", "1"]
-        #corrupted_char = (Choices(list(range(a, b)), burst)).result
         corrupted_char = [random.choice(list(range(a, b))) for _ in range(burst)]
-        #corrupted_char = choices.sequence(list(a, b), burst)
-        #corrupted_char = random.choice(list(a, b))
-        #corrupted_char = list.
-        #corrupted_char = random.sample(list(range(a, b)), min(burst, b - a))
-        #corrupted_char = random.choices(list(range(a, b)), k = burst)
-        #print(corrupted_char)
         for i in range(a, b):
-            for j in range(0, len(self.matrix_corrupted[0])):
+            for j in range(0, len(new_matrix[0])):
                 if i in corrupted_char:
-                    self.matrix_corrupted[i][j] = bits[random.randint(0, 1)]
-        return (self.matrix_corrupted)
-    
-    def BE_msg_matrix_text(self):
+                    new_matrix[i][j] = bits[random.randint(0, 1)]
+        return (new_matrix)
+
+    def BE_msg_matrix_text(self, matrix):
         msg = ""
-        for i in range(0, len(self.matrix_corrupted)):
+        for i in range(0, len(matrix)):
             character = ""
-            for j in range(0, len(self.matrix_corrupted[0])):
-                character += self.matrix_corrupted[i][j]
+            for j in range(0, len(matrix[0])):
+                character += matrix[i][j]
             num = int(character, 2)
             msg += chr(num)
         return (msg)
-
-'''
--- transmisor.py
+++ transmisor.py
@@ -1,15 +1,17 @@
+# -*- coding: utf-8 -*-
 # UART Communication Example with Raspberry Pi Pico W
 import random
 import machine
 import utime
 import ssd1306
 from DataLink import DataLink
-import errorDetection 
+from errorDetection import BurstError
 
 i2c = machine.I2C(0, scl=machine.Pin(21), sda=machine.Pin(20), freq=400000)
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 data_link_snd = DataLink() # Create object DataLink send
 data_link_rcv = DataLink() # Create object DataLink received
+burst = BurstError()
 message = "An enormous puppy was looking down at her with large round eyes, and feebly stretching out one paw, trying to touch her. Poor little thing! said Alice, in a coaxing tone, and she tried hard to whistle to it; but she was terribly frightened all the time at the thought that it might be hungry, in which case it would be very likely to eat her up in spite of all her coaxing."
 msg_corrupted = ""
 matrix = []
@@ -21,22 +23,22 @@ def sendMessage(serial, data_link, msg):
     myLed = machine.Pin("LED", machine.Pin.OUT)
     myLed.toggle()
 
-while (True):
-    while (count < 1000):
+while True:
+    while count <= 1000:
         uart = machine.UART(0, baudrate=115200, tx=0, rx=1)
         oled.fill(0)
         oled.text("transmitter", 0, 0)
         oled.text("Count: " + str(count), 0, 20)
         oled.show()
-        p_matrix = errorDetection.BE_msg_matrix(message)
-        p_matrix_corrupted = errorDetection.BE_generate_burst(p_matrix, 3, 6, random.randint(0, 4))
-        msg_corrupted = errorDetection.BE_msg_matrix_text(p_matrix_corrupted)
+        p_matrix = burst.BE_msg_matrix(message)
+        p_matrix_corrupted = burst.BE_generate_burst(p_matrix, 0, len(p_matrix), random.randint(0, 4))
+        msg_corrupted = burst.BE_msg_matrix_text(p_matrix_corrupted)
         del(p_matrix_corrupted)
         del(p_matrix)
         sendMessage(uart, data_link_snd, msg_corrupted) # Send data
         print("Original msg: " + message + " " + str(len(message)))
-        print("\n")
         print("Modified msg: " + msg_corrupted + " " + str(len(msg_corrupted)))
+        print("\n")
         utime.sleep_ms(1000)
         count += 1
         del(msg_corrupted)
